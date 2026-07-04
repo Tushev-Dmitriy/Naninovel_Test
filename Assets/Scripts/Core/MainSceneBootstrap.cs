a4f7e1ae043f96ccbbcf5d2885fedf5c955728a7
Dmitriy Tushev <tushev-dmitriy@mail.ru>
2026-07-04 16:03:40 +0500

    main scene setup
    create main scene and prepare project structure

---
+++ Assets/Scripts/Core/MainSceneBootstrap.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace NaninovelTest.Core
+{
+    public class MainSceneBootstrap : MonoBehaviour
+    {
+        [SerializeField] private Transform locationsRoot;
+        [SerializeField] private Transform gameplayUiRoot;
+        [SerializeField] private Transform location1Root;
+        [SerializeField] private Transform location2Root;
+        [SerializeField] private Transform location3Root;
+        [SerializeField] private Transform minigameRoot;
+    }
+}
---
+++ Assets/Scripts/Locations/LocationId.cs
@@ -0,0 +1,9 @@
+namespace NaninovelTest.Locations
+{
+    public enum LocationId
+    {
+        Location1 = 0,
+        Location2 = 1,
+        Location3 = 2
+    }
+}
